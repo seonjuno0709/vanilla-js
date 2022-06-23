cd93e41fda7123babddbfbb7727ad2bd75c4e618
Seon <76555222+seonjuno0709@users.noreply.github.com>
2022-06-23 13:41:32 +0900

    practice

---
+++ js/map.js
@@ -0,0 +1,6 @@
+new daum.roughmap.Lander({
+  timestamp: "1655958320472",
+  key: "2apqo",
+  mapWidth: "640",
+  mapHeight: "500",
+}).render();
---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "47e57327980d61d7e3128f9b2fc40a02";
+
+function anGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const nameContainer = document.querySelector("#weather span:first-child");
+      const weatherContainer = document.querySelector("#weather span:last-child");
+      nameContainer.innerHTML = data.name;
+      weatherContainer.innerHTML = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function anGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(anGeoOk, anGeoError);
